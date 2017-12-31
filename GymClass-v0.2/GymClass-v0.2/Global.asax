ad19908c549cac0aceed5bf9b4bcabe2367be942
Taleb Mosyri <taleb_n1@hotmail.com>
2017-12-31 11:56:08 +0100

    SetUpProject

---
+++ GymClass-v0.2/GymClass-v0.2/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GymClass_v0._2.MvcApplication" Language="C#" %>
---
+++ GymClass-v0.2/GymClass-v0.2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymClass_v0._2.Startup))]
+namespace GymClass_v0._2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
